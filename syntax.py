72ac225d0437179964ad52c24f2d5148b7271075
Olaki Edwin <olakiedwin@gmail.com>
2023-10-16 15:19:04 +0300

    Python Syntax

---
+++ syntax.py
@@ -0,0 +1,25 @@
+# Whitespace and Indentation
+# Define main function to print out something
+def main():
+    i = 1
+    max = 10
+    while (i < max):
+        print(i)
+        i = i + 1
+
+# Call function
+main()
+
+# Continuation of statements
+if (a == True) and (b == False) and
+    (c == True):
+    print("Continuation of Statements")
+
+# String literals
+s = 'This is a string'
+print(s)
+s = "Another string using double quotes"
+print(s)
+s = ''' string can span
+        multiple line '''
+print(s)
