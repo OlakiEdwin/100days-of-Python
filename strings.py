3dee32f6655df41edabd423ab0e98d9200885bf6
Olaki Edwin <olakiedwin@gmail.com>
2023-10-16 15:40:59 +0300

    Multiline comments

---
+++ strings.py
@@ -0,0 +1,21 @@
+message = 'Hi there, this is a string with single quotes'
+print(message)
+
+message = "Hi there, this is a string with double quotes"
+print(message)
+
+message = "It's a string"
+print(message)
+
+message = '"Beautiful is better than ugly", says Tim Peters'
+print(message)
+
+# Creating multiline strings
+student_details = ''' 
+Campus Details: MUBS Nakawa
+    -Student name: Olaki Edwin
+    -Student number: 2100706601
+    -Phone number: 0787499401
+    -Programme: BBC
+'''
+print(student_details)
