07e2169f0949a00755fd77d541a3ba1d69d4ea4b
Olaki Edwin <olakiedwin@gmail.com>
2024-01-07 12:53:55 +0300

    CommentsCopy

---
+++ CommentsCopy.py
@@ -0,0 +1,14 @@
+# Python Block Comments
+
+salary = salary *1.02 # Python inline comments
+
+# Python DocStrings
+""" One line DocStrings """
+
+""" Multi-line DocStrings
+ My 
+ Name
+ Is
+ Olaki
+ Edwin 
+ """
