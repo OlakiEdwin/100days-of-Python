d67451739cccead267e42c493a27d46e444d5264
Olaki Edwin <olakiedwin@gmail.com>
2024-01-18 12:09:44 +0300

    Loops

---
+++ Loops.py
@@ -0,0 +1,17 @@
+cars = ['toyota', 'bmw', 'tesla', 'mazda', 'mercedes', 'honda']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.capitalize())
+
+
+
+names = ['olaki', 'edwin', 'oryokot']
+
+for name in names:
+    if name == 'olaki':
+        print(name.upper())
+    else:
+        print(name.capitalize())
