955fa68f7ea0068cc277f37e187ba014d938fe96
Olaki Edwin <olakiedwin@gmail.com>
2024-01-23 17:48:43 +0300

    WhileLoops & Functions

---
+++ Functions.py
@@ -0,0 +1,19 @@
+age = 18
+age2 = 17
+
+def check_age():
+    print('Check age function was invoked')
+
+check_age()
+
+if age < 18:
+    print('Not an adult')
+
+else:
+    print('An adult')
+
+if age2 < 18:
+    print('Not an adult')
+
+else:
+    print('An adult')
