3573a48ed31a10f7de972ef80c840dd2af3cd809
Olaki Edwin <olakiedwin@gmail.com>
2024-01-24 14:36:20 +0300

    Creating behaviours

--- Classes&Objects.py
+++ Classes&Objects.py
@@ -3,8 +3,19 @@ class Person:
         self.name = name
         self.age = age
 
+    def walk(self):
+            print(self.name  + ' is walking...')
+
+    def speak(self):
+            print('Hello my name is ' + self.name + ' and I am ' + str(self.age) + ' years old')
+
 john = Person('John', 22)
 mariam = Person('Mariam', 20)
 
 print(john.name + ' ' + str(john.age))
+john.speak()
+john.walk()
+
 print(mariam.name + ' ' + str(mariam.age))
+mariam.speak()
+mariam.walk()
