a3f02d1bdca04f74f27ebec16b0f0d0f4b9b90d9
Olaki Edwin <olakiedwin@gmail.com>
2024-01-31 17:25:39 +0300

    function that accepts a string and a single character, and returns an integer

--- Challenges.py
+++ Challenges.py
@@ -1,10 +1,19 @@
-def total_goals(laLiga, copaDelRey, championsLeague):
-    return laLiga + copaDelRey + championsLeague
+# def total_goals(laLiga, copaDelRey, championsLeague):
+#     return laLiga + copaDelRey + championsLeague
+#
+# goals_in_la_liga = 5
+# goals_in_copa_del_rey = 10
+# goals_in_champions_league = 15
+#
+# total_goals_scored = total_goals(goals_in_la_liga, goals_in_copa_del_rey, goals_in_champions_league)
+#
+# print(total_goals_scored)
 
-goals_in_la_liga = 5
-goals_in_copa_del_rey = 10
-goals_in_champions_league = 15
 
-total_goals_scored = total_goals(goals_in_la_liga, goals_in_copa_del_rey, goals_in_champions_league)
 
-print(total_goals_scored)
+def str_count(strng, letter):
+    return strng.count(letter)
+
+print(str_count("Hello", 'o'))
+print(str_count("Hello", 'l'))
+print(str_count("", 'z'))
